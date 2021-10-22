ee8c3f9660c7621a7c2536933d03dcc91dedf0e6
kirilllan <s2100138@edu.bc.fi>
2021-10-22 14:44:37 +0300

    connection test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>email sender</title>
+  <title>_</title>
   <style>
   * {box-sizing: border-box;}
   body {font-family: 'Courier New', sans-serif;background-color: NavajoWhite; color: Peru;
